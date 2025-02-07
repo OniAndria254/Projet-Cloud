10c58744039b78dbd45e3eddaa05243aa42f83ec
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2025-02-08 00:31:38 +0300

    Role user firestore

--- SpringUI/src/main/java/itu/p16/crypto/firebase/listener/firestore/UserFirestoreListener.java
+++ SpringUI/src/main/java/itu/p16/crypto/firebase/listener/firestore/UserFirestoreListener.java
@@ -56,7 +56,7 @@ public class UserFirestoreListener {
 
         if (!user.isSyncFromFirestore()) {
             user.setIdTentatives(1);
-            user.setIdRole(2);
+//            user.setIdRole(2);
             user.setSyncFromFirestore(true);
             userRepository.save(user);
         }
@@ -66,6 +66,7 @@ public class UserFirestoreListener {
     private void handleModifiedDocument(DocumentSnapshot documentSnapshot) {
         UsersDocument document = documentSnapshot.toObject(UsersDocument.class);
         Users user = convertToEntity(document);
+//        System.out.println(document.getIdRole() + " " + user.getIdRole());
 
         Optional<Users> existingUser = userRepository.findById(Math.toIntExact(user.getIdUsers()));
         if (existingUser.isPresent()) {
@@ -74,7 +75,7 @@ public class UserFirestoreListener {
             if (!existing.equals(user) && !user.isSyncFromFirestore()) {
                 user.setSyncFromFirestore(true);
                 user.setIdTentatives(1);
-                user.setIdRole(2);
+//                user.setIdRole(2);
                 userRepository.save(user);  
             }
         }
